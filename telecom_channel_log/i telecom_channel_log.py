710f4281b2630568f8891df396a35faaca89678f
竹永康 <gqylpy@outlook.com>
2025-03-04 21:02:43 +0800

    1.0.4

--- telecom_channel_log/i telecom_channel_log.py
+++ telecom_channel_log/i telecom_channel_log.py
@@ -115,7 +115,7 @@ def logger(msg, *args, **extra):
     if isinstance(msg, (str, unicode)):
         msg = (msg % args)[:1000]
     elif isinstance(msg, (dict, list, tuple)):
-        msg = jsonx.dumps(OmitLongString(msg), ensure_ascii=False)
+        msg = OmitLongString(msg)
 
     f_back = inspect.currentframe().f_back
     level = f_back.f_code.co_name
